27f08096fb8b6f08b6401fe09507deb9dfba0a62
Mithula <mithula.tbm@gmail.com>
2023-10-11 19:48:38 +0530

    a

--- flip.js
+++ flip.js
@@ -1,10 +1,13 @@
 function onloadPage(){
     document.getElementById('coupon').style.visibility="visible";
     document.getElementById('category').style.opacity="0.7";
+
     
 }
 
 function closecoupon(){
     document.getElementById('coupon').style.visibility="hidden";
     document.getElementById('category').style.opacity="1";
+  
+
 }
